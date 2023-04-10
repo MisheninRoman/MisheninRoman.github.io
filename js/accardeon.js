5a296c38ced4c4865146589862ba1c0c5c2c60b1
Roman Mishenin <misheninrb@yandex.ru>
2023-04-10 15:36:40 +0300

    somechenges in style 'project.block'

--- js/accardeon.js
+++ js/accardeon.js
@@ -1,20 +1,41 @@
-// Получить все кнопки аккордеона
-var acc = document.getElementsByClassName("accordion");
+"use strict";
+// // Получить все кнопки аккордеона
+// var acc = document.getElementsByClassName("accordion");
 
-// Добавить обработчик события click к каждой кнопке аккордеона
-for (var i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    // Переключить класс "active" для текущей кнопки
-    this.classList.toggle("active");
+// // Добавить обработчик события click к каждой кнопке аккордеона
+// for (var i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function () {
+//     // Переключить класс "active" для текущей кнопки
+//     this.classList.toggle("active");
 
-    // Найти следующий элемент панели (следующий элемент после кнопки)
-    var panel = this.nextElementSibling;
+//     // Найти следующий элемент панели (следующий элемент после кнопки)
+//     var panel = this.nextElementSibling;
 
-    // Если панель отображается, скрыть ее, и наоборот
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+//     // Если панель отображается, скрыть ее, и наоборот
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+  $(document).ready(function () {
+    $(".panel").hide(); // Скрыть все панели при загрузке страницы
+
+    $(".accordion").click(function () {
+      if ($(this).hasClass("active")) {
+        // Если текущий аккордион уже открыт
+        $(this).removeClass("active"); // Закрыть текущий аккордион
+        $(this).next(".panel").slideUp(250); // Скрыть его панель с анимацией
+      } else {
+        // Если текущий аккордион закрыт
+        $(".accordion.active").removeClass("active"); // Закрыть предыдущий аккордион
+        $(".panel").slideUp(250); // Скрыть его панель с анимацией
+        $(this).addClass("active"); // Открыть текущий аккордион
+        $(this).next(".panel").slideDown(250); // Показать его панель с анимацией
+      }
+    });
   });
-}
+});
