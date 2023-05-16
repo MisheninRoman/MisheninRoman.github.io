c3bfb9305efd1aeabad71605d242bec7548eb3f9
Roman Mishenin <misheninrb@yandex.ru>
2023-05-16 10:07:37 +0300

    somechange in form.js

--- js/form.js
+++ js/form.js
@@ -1,20 +1,36 @@
-'use strict'
+"use strict";
+
+let isFormSubmitted = false;
 
 function sendMessage() {
-    const name = document.forms["contact-form"]["name"].value;
-    const email = document.forms["contact-form"]["email"].value;
-    const phone = document.forms["contact-form"]["phone"].value;
-    const message = document.forms["contact-form"]["message"].value;
-    const url = `https://api.telegram.org/bot5849871032:AAE4V9B8TW-ltvxv8e4ix9Ubifyo0jMD2Xk/sendMessage?chat_id=@RomaNotific&text=
-        Имя: ${name}
-        Email: ${email}
-        Номер телефона: ${phone}
-        Сообщение: ${message}`
-    const Http = new XMLHttpRequest();
-    Http.open("GET", url);
-    Http.send();
+  const name = document.forms["contact-form"]["name"].value;
+  const email = document.forms["contact-form"]["email"].value;
+  const phone = document.forms["contact-form"]["phone"].value;
+  const message = document.forms["contact-form"]["message"].value;
 
-    Http.onreadystatechange = (e) => {
-        console.log(Http.responseText)
+  if (!name || !email || !phone || !message) {
+    alert("Заполните все строки");
+    return;
+  }
+
+  const url = `https://api.telegram.org/bot5849871032:AAE4V9B8TW-ltvxv8e4ix9Ubifyo0jMD2Xk/sendMessage?chat_id=@RomaNotific&text=
+        Имя: ${name}%0D%0A
+        Email: ${email}%0D%0A
+        Номер телефона: ${phone}%0D%0A
+        Сообщение: ${message}`;
+  const Http = new XMLHttpRequest();
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText);
+    if (
+      Http.readyState === XMLHttpRequest.DONE &&
+      Http.status === 200 &&
+      !isFormSubmitted
+    ) {
+      isFormSubmitted = true;
+      alert("Заявка успешно отправлена");
     }
+  };
 }
